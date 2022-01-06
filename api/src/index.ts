64f17918ecb7156c6177be74b374f907357aec97
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-01-05 19:27:17 -0600

    Intentando entenderle a typescript

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,59 +1,15 @@
 import { ApolloServer } from 'apollo-server'
+import path from 'path'
+import { readFileSync } from 'fs'
 
-const typeDefs = `
-
-type AvocadoAttributes {
-    description: String!
-    shape: String!
-    hardiness: Float!
-    taste: String!
-}
-
-type Avocado {
-    name: String!
-    id: ID!
-    sku: String!
-    price: Float!
-    image: String!
-    attributes: AvocadoAttributes!
-}
-
-type Cart {
-    id: ID!
-    avocadoId: ID!
-}
-
-type Query {
-    getAvocados: [Avocado]
-    info: String!
-}
-
-type Mutation {
-    createAvocado(name: String!, hardiness: Float!, description: String!, shape: String!, taste: String!, sku: String!, image: String!, price: Float!): Avocado
-    deleteAvocado(id: ID!): Avocado
-}
-`
+const typeDefs = readFileSync(
+    path.join(__dirname, 'models', 'schema.graphql'),
+    {
+        encoding: 'utf-8'
+    }
+)
 
 const resolvers = {
-    Query: {
-        info: () => 'This is the API of a Hackernews Clone',
-        getAvocados: () => Avocado.find()
-    },
-    Mutation: {
-        createAvocado: (parent, args) => {
-            const avocado = new Avocado({
-                name: args.name,
-                hardiness: args.hardiness,
-                description: args.description,
-                shape: args.shape,
-                taste: args.taste,
-                sku: args.sku,
-                image: args.image,
-                price: args.price
-            })
-            return avocado.save()
-        }
-    }
 }
 
 const server = new ApolloServer({
