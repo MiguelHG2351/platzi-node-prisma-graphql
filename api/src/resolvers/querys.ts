3aa7f35e903dfec326566a62e067915863d85c4c
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-01-09 20:08:11 -0600

    querys avanzadas

--- api/src/resolvers/querys.ts
+++ api/src/resolvers/querys.ts
@@ -1,15 +1,25 @@
 // import { Avocado } from "./base/avocado.model"
-import type { Avocado } from '@prisma/client'
+import type { Avocado, Prisma } from '@prisma/client'
 import type { context } from './types'
 
 export const querys = {
-    async find(root: unknown, args: unknown, context: context): Promise<Avocado[]> {
-        return await context.orm.avocado.findMany();
+    async find(root: unknown, args: {skip?: number, take?:number, where: Prisma.AvocadoWhereInput}, context: context): Promise<Avocado[]> {
+        return await context.orm.avocado.findMany({
+            include: {
+                attributes: true
+            },
+            skip: args.skip,
+            take: args.take,
+            where: args.where
+        });
     },
     async findOne(root: unknown, args: { id: number }, context: context): Promise<Avocado | null> {
         return  await context.orm.avocado.findUnique({
             where: {
-                id: args.id
+                id: args.id,
+            },
+            include: {
+                attributes: true
             }
         });   
     }
