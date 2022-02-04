1fa2fcd9c4333ee6ce1737eec23b13635298596b
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-02-04 09:16:27 -0600

    ignore dist

--- api/dist/api/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const index_1 = (0, tslib_1.__importDefault)(require("./src/index"));
-(0, index_1.default)();
-//# sourceMappingURL=index.js.map
